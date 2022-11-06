7df9bee43d67b9c503ea6602284d8ac323d80e9f
Paull Stanley <paull.stanley@me.com>
2022-11-05 20:24:54 -0400

    Initial Commit

---
+++ NotMe/NotMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotMeApp.swift
+//  NotMe
+//
+//  Created by Paull Stanley on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NotMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
